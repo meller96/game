eee1713f2f7aa68a5ffddad05b3cff6d62a1f32d
meller96 <meller70@gmail.com>
2017-12-16 21:26:38 +0300

    game is complete

---
+++ main.js
@@ -0,0 +1,118 @@
+
+var mainState = {
+    preload: function () {
+        game.load.image('bird', 'assets/bird.png');
+        game.load.image('pipe', 'assets/pipe.png');
+        game.load.audio('jump', 'assets/jump.wav');
+    },
+
+    create: function () {
+        this.jumpSound = game.add.audio('jump');
+        this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+        this.score = 0;
+        this.labelScore = game.add.text(20, 20, "0", {
+            font: "30px Arial",
+            fill: "#ffffff"
+        });
+        game.stage.backgroundColor = '#336600';
+      
+        this.pipes = game.add.group();
+   
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+
+   
+        this.bird = game.add.sprite(100, 245, 'bird');
+
+    
+        game.physics.arcade.enable(this.bird);
+
+ 
+        this.bird.body.gravity.y = 1000;
+
+       
+        var spaceKey = game.input.keyboard.addKey(
+            Phaser.Keyboard.SPACEBAR);
+        spaceKey.onDown.add(this.jump, this);
+
+    },
+    update: function () {
+        if (this.bird.angle < 20)
+            this.bird.angle += 1;
+        game.physics.arcade.overlap(this.bird, this.pipes, this.restartGame, null, this);
+        
+        if (this.bird.y < 0 || this.bird.y > 490)
+            this.restartGame();
+    },
+  
+    jump: function () {
+        if (this.bird.alive == false)
+            return;
+      
+        var animation = game.add.tween(this.bird);
+
+    
+        animation.to({
+            angle: -20
+        }, 100);
+
+       
+        animation.start();
+     
+        this.bird.body.velocity.y = -350;
+        this.jumpSound.play();
+    },
+    addOnePipe: function (x, y) {
+   
+        var pipe = game.add.sprite(x, y, 'pipe');
+
+     
+        this.pipes.add(pipe);
+
+     
+        game.physics.arcade.enable(pipe);
+
+        pipe.body.velocity.x = -200;
+
+
+        pipe.checkWorldBounds = true;
+        pipe.outOfBoundsKill = true;
+    },
+    addRowOfPipes: function () {
+        this.score += 1;
+        this.labelScore.text = this.score;
+     
+        var hole = Math.floor(Math.random() * 5) + 1;
+
+        
+        for (var i = 0; i < 8; i++)
+            if (i != hole && i != hole + 1)
+                this.addOnePipe(400, i * 60 + 10);
+    },
+   
+    restartGame: function () {
+        game.physics.arcade.overlap(
+            this.bird, this.pipes, this.hitPipe, null, this);
+       
+        game.state.start('main');
+    },
+    hitPipe: function () {
+        
+        if (this.bird.alive == false)
+            return;
+
+    
+        this.bird.alive = false;
+
+        game.time.events.remove(this.timer);
+
+        this.pipes.forEach(function (p) {
+            p.body.velocity.x = 0;
+        }, this);
+    },
+};
+
+
+var game = new Phaser.Game(400, 490);
+
+game.state.add('main', mainState);
+game.state.start('main');
